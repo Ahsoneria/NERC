0e7db4c9cd40f4cb41e2d8d1f46071b4660e8a35
Ahsoneria <soneria.ayush@gmail.com>
2022-11-19 05:47:16 -0700

    took arguments into account

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ import os
 from api import NercAPI
 
 
-def create_app():
+def create_app(host, port):
     app = Flask(__name__, instance_relative_config=True)
     api = NercAPI()
 
@@ -17,13 +17,13 @@ def create_app():
     @app.route('/nerc_home', methods=['POST', 'GET'])
     def nerc_home():
         if request.method == 'GET':
-            return render_template('index.html')
+            return render_template('index.html', host=host, port=port)
         else:
             input_text=request.form['input_text']
             result = api.get_nerc_result(input_text)
             return render_template('result.html', 
-                               input_text=input_text,
-                               nerc_output=result)
+                                   input_text=input_text,
+                                   nerc_output=result)
 
     return app
 
@@ -32,10 +32,9 @@ def run():
     parser.add_argument('-p', '--port', help='port', type=int, default=9093)
     parser.add_argument('-host', '--host', help='host', default='localhost')
     args = parser.parse_args()
-    app = create_app()
+    app = create_app(args.host, args.port)
     # app.run(host=args.host, port=args.port)
     serve(app, host=args.host, port=args.port)
 
 if __name__ == '__main__':
-    os.environ['KMP_DUPLICATE_LIB_OK']='True'
     run()
